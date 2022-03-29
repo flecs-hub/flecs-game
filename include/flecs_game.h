5eb8860efff74e1603d54f6ac3897c23cedc2080
Sander Mertens <sander.mertens8@gmail.com>
2022-03-29 00:41:40 -0700

    Initial commit

---
+++ include/flecs_game.h
@@ -0,0 +1,21 @@
+#ifndef GAME_H
+#define GAME_H
+
+/* This generated file contains includes for project dependencies */
+#include "flecs-game/bake_config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern ECS_DECLARE(EcsCameraController);
+
+FLECS_GAME_API
+void FlecsGameImport(ecs_world_t *world);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
